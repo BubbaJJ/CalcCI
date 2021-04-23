8e5a6380efadb63d2dcc5f6fc632371d81663ffe
Jim Kaya <jim.kaya@gmail.com>
2021-04-23 11:47:37 +0200

    first commit

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+class Calc:
+    def add(x, y):
+        return x + y
+
+    def sub(x, y):
+        return x - y
+
+    def mul(x, y):
+        return x * y
+
+    def div(x, y):
+        return x / y
