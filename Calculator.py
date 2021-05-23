1d8557aa748cd2675528f917a021610c0416d526
BubbaJJ <55495429+BubbaJJ@users.noreply.github.com>
2021-05-23 17:52:50 +0200

    added syspath

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,34 @@
-class Calc:
-    # Addition
-    def add(x, y):
-        return x + y
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Calculator import Calc
 
-    # Subtraction
-    def sub(x, y):
-        return x - y
 
-    # Multiplication
-    def mul(x, y):
-        return x * y
+def test_add():
+    assert 13 == Calc.add(5, 8)
 
-    # Division
-    def div(x, y):
-        return x / y
 
-    # Squared / Power
-    def power(x, y):
-        return pow(x, y)
+def test_sub():
+    assert 13 == Calc.sub(19, 6)
 
-    # Modulus
-    def modulo(x, y):
-        return x % y
 
-    # Checking number of letters in string
-    # Returns true if counter is above 9
-    def countChars(str):
-        counter = sum(1 for i in str if i.isalpha())
-        return bool(counter > 9)
+def test_mul():
+    assert 39 == Calc.mul(13, 3)
+
+
+def test_div():
+    assert 12 == Calc.div(48, 4)
+
+
+def test_power():
+    assert 9 == Calc.power(3, 2)
+
+
+def test_modulo():
+    assert 0 == Calc.modulo(10, 2)
+
+
+def test_countChars():
+    assert True is Calc.countChars("KalleAnkaP")
+    assert False is Calc.countChars("KalleAnka")
+    assert False is Calc.countChars("KalleAnka ")
